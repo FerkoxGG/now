cd6371337ca06bc9a1f22f62ca6f425fa271d815
mauricio pineda <map1900@outlook.com>
2021-08-27 17:57:03 -0400

    added allotments model, controller, view (index)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_214255) do
+ActiveRecord::Schema.define(version: 2021_08_27_203536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "allotments", force: :cascade do |t|
+    t.decimal "total"
+    t.string "name"
+    t.integer "redeemed_number"
+    t.date "expire_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_allotments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_08_24_214255) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "allotments", "users"
 end
