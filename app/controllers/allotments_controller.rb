19980bd64e403c32abf1993cf47c3fd1c561770b
daniela saavedra <daniela.ocampos1@gmail.com>
2021-08-31 17:17:22 -0400

    allotment show

--- app/controllers/allotments_controller.rb
+++ app/controllers/allotments_controller.rb
@@ -1,9 +1,22 @@
 class AllotmentsController < ApplicationController
+  before_action :find_allotment, except: [ :index, :new, :create ]
+
   def index
     @allotments = Allotment.all
   end
 
   def show
+    @allotments = Allotment.new
+  end
 
+
+private
+
+  def allotment_params
+    params.require(:allotment).permit(:name, :total, :expire_date, :user_id, :redeemed_number)
+  end
+
+  def find_allotment
+    @allotment = Allotment.find(params[:id])
   end
 end
