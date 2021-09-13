595fbaeebbfb4e730cef96b397a71d92c05cee79
mauricio pineda <map1900@outlook.com>
2021-09-13 19:04:02 -0300

    Revert "added contact us view, controller and model also  added static thanks page and route"
    This reverts commit d278695a15f53c3bb4b731614909f3d554113761.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-  get 'messages/create'
   root to: 'pages#home'
-  get '/thanks', to: 'pages#thanks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :allotments, only: [:index, :show] do
@@ -11,5 +8,4 @@ Rails.application.routes.draw do
   resources :offers, only: [:show] do
     resources :purchases, only: [:create]
   end
-  resources :messages, only: [ :new, :create ]
 end
