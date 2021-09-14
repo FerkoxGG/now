6cf14b8464e55dc6df6360c05228bd373acfd3d8
ÁLVARO MEDINA <alvaro.e.medina.a@gmail.com>
2021-09-14 16:52:05 -0300

    front mejorado

---
+++ db/migrate/20210914195020_create_messages.rb
@@ -0,0 +1,13 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.string :user
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
