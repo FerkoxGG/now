a688d56d161c05eaa412b4cc3a9bfc153eaa2288
Fernando Antonio Gonzalez Gaete <fernandogonzalezgaete@gmail.com>
2021-09-14 17:00:21 -0300

    solved doble route message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-  get 'messages/create'
-  root to: 'pages#home'
+root to: 'pages#home'
   get '/thanks', to: 'pages#thanks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
